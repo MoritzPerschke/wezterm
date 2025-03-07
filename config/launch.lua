5e23ce2479a0edc4b2ae01a6c4aa8d6f4995845d
Moritz Perschke <perschkemoritz@gmail.com>
2025-03-07 12:06:17 +0100

    changes after installing on pop os + new backdrops

--- config/launch.lua
+++ config/launch.lua
@@ -5,28 +5,8 @@ local options = {
    launch_menu = {},
 }
 
-if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
-   options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
-      { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      {
-         label = 'Git Bash',
-         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
-      },
-   }
-elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-   options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
-      { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
-   }
-elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
+if platform.is_linux then
+   options.default_prog = { 'bash', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { 'fish', '-l' } },
